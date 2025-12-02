0086ba9557f77736c895e0532fb73cd65ea9f21d
Francesco Foresta <fr.foresta@gmail.com>
2025-12-02 22:57:32 +0100

    Fix mail (again)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import Mail from './Mail.js';
 import Users from './Users.js';
 import { BrowserRouter, Outlet, Routes, Route } from 'react-router-dom';
 import { Fab } from '@mui/material';
