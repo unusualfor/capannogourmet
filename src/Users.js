2f671dc3c386fa80e8674de69f936a9854ee755a
Francesco Foresta <fr.foresta@gmail.com>
2025-12-02 22:35:12 +0100

    Changed Users.js (unused)

--- src/Users.js
+++ src/Users.js
@@ -7,7 +7,7 @@ function Users() {
   const { id } = useParams();
   const [users, setUsers] = useState([]);
   useEffect(() => {
-    fetch(`https://51.75.16.232:8087/api/draghi`)
+    fetch(`https://francescoforesta.com:8087/api/draghi`)
       .then((res) => res.json())
       .then((data) => {
         setUsers(data);
